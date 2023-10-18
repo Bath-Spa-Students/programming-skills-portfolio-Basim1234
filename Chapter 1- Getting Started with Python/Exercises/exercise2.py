0bd3ab298bb5c3df715e896e976cd405e136c67e
Basim1234 <148310552+Basim1234@users.noreply.github.com>
2023-10-18 10:35:53 +0400

    chapter 1 exercises

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise2.py
@@ -0,0 +1,4 @@
+#Write a Python program to get the Python version you are using.
+import sys
+print("python version")
+print(sys.version)
---
+++ Chapter 1- Getting Started with Python/Exercises/exercise4.py
@@ -0,0 +1,7 @@
+#Write three strings in different variables and print the output as one 
+first_name= "muhammad"
+middle_name= "baism"
+last_name= "ashfaq"
+full_name= f"{first_name} {middle_name} {last_name}"
+print(full_name)
+
