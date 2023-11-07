b97a6e291433a4408c7eb94bbb673b1323aaed36
Basim1234 <148310552+Basim1234@users.noreply.github.com>
2023-11-07 10:31:47 +0400

    chapter 3 exercises

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,11 @@
+#printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+names = ['basim', 'ali', 'john']
+
+msg = f"Hello, {names[0].title()}!"
+print(msg)
+
+msg = f"Hello, {names[1].title()}!"
+print(msg)
+
+msg = f"Hello, {names[2].title()}!"
+print(msg)
---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,5 @@
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+favorite_transportation = ['car', "boat"]
+
+print("I go to work using {}".format(favorite_transportation[0]))
+print("I travel overseas using {}".format(favorite_transportation[1]))
