3cffa463db7ac02392e23f1b2c0e4ed64842866a
Basim1234 <148310552+Basim1234@users.noreply.github.com>
2023-11-08 09:40:10 +0400

    chapter 4 exercises

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,19 @@
+#Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+
+#•If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+
+#•If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+#•Write one version of this program that runs the if block and another that runs the else block.
+
+alien_colour= 'red'
+if alien_colour == 'red':
+    print("you just earned five points!")
+else:
+     print ("you just earned 10 points")
+     
+alien_colour= 'blue'
+if alien_colour == 'green':
+    print("you just earned five points!")
+else:
+     print ("you just earned 10 points")
---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,20 @@
+#Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+
+#•Make a list of your three favorite fruits and call it favorite_fruits.
+
+#•Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block 
+
+#should print a statement,such as You really like bananas!
+
+favorite_fruits = ['mango', 'banana', 'apple']
+
+if 'grapes' in favorite_fruits:
+    print("You really like grapes!")
+if 'apple' in favorite_fruits:
+    print("You really like apple!")
+if 'guava' in favorite_fruits:
+    print("You really like guava!")
+if 'dragonfruit' in favorite_fruits:
+    print("You really like dragonfruit!")
+if 'mango' in favorite_fruits:
+    print("You really like mango!")
